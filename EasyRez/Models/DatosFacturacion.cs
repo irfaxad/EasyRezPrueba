92a7c18d124c57474ccdbbcf4a2c2b57032d3217
Joan Olvera <irfaxad@gmail.com>
2022-08-31 17:22:31 -0500

    Desarrollo de aspecto visual y correcciones en puerto de salida.

--- EasyRez/Models/DatosFacturacion.cs
+++ EasyRez/Models/DatosFacturacion.cs
@@ -1,42 +1,63 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace EasyRez.Models
 {
     public class DatosFacturacion
     {
-        
         public int IdDatoFacturacion { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "Requerido.")]
+
+        [Range(1, int.MaxValue, ErrorMessage = "Se debe indicar el Tipo de Persona.")]
         public int TipoPersona { get; set; }
 
-        [Required(ErrorMessage = "Requerido.")]
+        [Required(ErrorMessage = "El RFC es obligatorio.")]
         public string? RFC { get; set; }
-        [Required(ErrorMessage = "Requerido.")]
+
+        [StringLength(250, ErrorMessage = "La Razón Social no puede tener más de 250 caracteres.")]
+        [Required(ErrorMessage = "La Razón Social es obligatoria.")]
         public string? RazonSocial { get; set; }
-        [Required(ErrorMessage = "Requerido.")]
+
+        [Required(ErrorMessage = "El Nombre es obligatorio.")]
+        public string? Nombres { get; set; }
+
+        public string? ApPaterno { get; set; }
+
+        public string? ApMaterno { get; set; }
+
+        [EmailAddress(ErrorMessage = "Se debe ingresar un correo válido.")]
+        [Required(ErrorMessage = "Se debe capturar un correo.")]
         public string? Correo { get; set; }
         public int MetodoPago { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "Requerido.")]
 
+        [Range(1, int.MaxValue, ErrorMessage = "Se debe seleccionar una opción.")]
         public int UsoCFDI { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "Requerido.")]
+
+        [Range(1, int.MaxValue, ErrorMessage = "Se debe seleccionar una opción.")]
         public int RegimenFiscal { get; set; }
         public int IdDireccion { get; set; }
         public bool EsSucursal { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "Requerido.")]
+
+        [Range(1, int.MaxValue, ErrorMessage = "Se debe seleccionar una opción.")]
         public int TipoEntidadTributaria { get; set; }
         public int IdEntidadTributaria { get; set; }
 
         [Display(Name = "Metodo Pago")]
         public string? StrMetodoPago { get; set; }
+
         [Display(Name = "Uso CFDI Clave")]
         public string? StrUsoCFDIClave { get; set; }
+
         [Display(Name = "Uso CFDI Descripcion")]
         public string? StrUsoCFDIDescripcion { get; set; }
+
         [Display(Name = "Tipo Persona")]
         public string? StrTipoPersona { get; set; }
+
         [Display(Name = "Regimen Fiscal Clave")]
         public string? StrRegimenFiscalClave { get; set; }
+
         [Display(Name = "Regimen Fiscal Descripción")]
         public string? StrRegimenFiscalDescripcion { get; set; }
 
@@ -51,5 +72,45 @@ namespace EasyRez.Models
             StrRegimenFiscalDescripcion = String.Empty;
         }
 
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ValidaRFCAttribute : ValidationAttribute
+    {
+        public String Nombre { get; set; }
+        public String ApPaterno { get; set; }
+        public String ApMaterno { get; set; }
+        public String RazonSocial { get; set; }
+        public String RFC { get; set; }
+
+        public ValidaRFCAttribute(String nombre, String apPaterno, String apMaterno, String razonSocial, String rfc)
+        {
+            Nombre = nombre;
+            ApPaterno = apPaterno;
+            ApMaterno = apMaterno;
+            RazonSocial = razonSocial;
+            RFC = rfc;
+        }
+
+        public override bool IsValid(object value)
+        {
+            Type objectType = value.GetType();
+
+            PropertyInfo[] RFCfields = objectType.GetProperties().Where(propertyInfo => propertyInfo.Name == Nombre || propertyInfo.Name == ApPaterno || propertyInfo.Name == ApMaterno || propertyInfo.Name == RazonSocial || propertyInfo.Name == RFC).ToArray();
+
+            if (RFCfields.Count() < 2)
+            {
+                throw new ApplicationException("ValidaRFCAttribute error on " + objectType.Name);
+            }
+
+            Boolean isValid = true;
+
+            if (!Convert.ToString(RFCfields[0].GetValue(value, null)).Equals("PEPE"))
+            {
+                isValid = false;
+            }
+
+            return base.IsValid(value);
+        }
     }
 }
